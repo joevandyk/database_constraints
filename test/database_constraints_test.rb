3d7400ec5d6cde7080a7647bbc49537be5eea427
Francesc Esplugas <fesplugas@intraducibles.net>
2009-04-25 10:37:43 +0200

    Refactored tests.

--- test/database_constraints_test.rb
+++ test/database_constraints_test.rb
@@ -60,79 +60,98 @@ class DatabaseConstraintsTest < Test::Unit::TestCase
     CreateTables.down
   end
 
-  def test_should_create_a_user_and_a_post
-    user = User.create
-    assert_equal 1, User.count
-    post = Post.create
-    assert_equal 1, Post.count
-  end
+  def test_should_add_email_check
 
-  def test_should_add_email_check_to_users
     ActiveRecord::Migration.add_email_check :users, :email
-    user = User.new
-    user.email = 'invalid@email'
-    assert user.valid?
-    begin
-      user.save
-    rescue Exception => error
-      assert_match /PGError/, error.message
-      assert_match /violates check constraint "valid_email"/, error.message
+
+    valid_emails = %w( john@example.com john@example.co.uk john@example.es )
+    valid_emails.each do |email|
+      User.create :email => email
     end
-    user.email = 'valid@email.com'
-    assert user.valid?
-    user.save
-    assert_equal 1, User.count
+    assert_equal valid_emails.size, User.count
+
+    %w( invalid@email invalid invalid.com ).each do |email|
+
+      begin
+        User.create :email => email
+      rescue Exception => error
+        assert_match /PGError/, error.message
+        assert_match /violates check constraint "valid_email"/, error.message
+      end
+
+    end
+
   end
 
-  def test_should_add_login_check_to_users
+  def test_should_add_login_check
+
     ActiveRecord::Migration.add_login_check :users, :login
-    user = User.new
-    user.login = "my-login"
-    assert user.valid?
+
+    User.create :login => "mylogin"
+    assert_equal 1, User.count
+
     begin
-      user.save
+      User.create :login => 'my-login?'
     rescue Exception => error
       assert_match /PGError/, error.message
       assert_match /violates check constraint "valid_login"/, error.message
     end
-    user.login = "mylogin"
-    assert user.valid?
-    user.save
-    assert_equal 1, User.count
+
   end
 
   def test_should_add_uniqueness_check
+
     ActiveRecord::Migration.add_uniqueness_check :users, :login
+
     user = User.create(:login => 'fesplugas')
+
     begin
       User.create(:login => 'fesplugas')
     rescue Exception => error
       assert_match /PGError/, error.message
       assert_match /duplicate key value violates unique constraint "unique_login"/, error.message
     end
+
   end
 
   def test_should_add_length_check
+
     ActiveRecord::Migration.add_length_check :users, :login, :is => 8
+
     user = User.create(:login => 'esplugas')
-    begin
-      User.create(:login => 'fesplugas')
-    rescue Exception => error
-      assert_match /PGError/, error.message
-      assert_match /violates check constraint "valid_login"/, error.message
+    assert_equal 1, User.count
+
+    %w( fesplugas f ).each do |login|
+
+      begin
+        User.create :login => login
+      rescue Exception => error
+        assert_match /PGError/, error.message
+        assert_match /violates check constraint "valid_login"/, error.message
+      end
+
     end
+
   end
 
   def test_should_add_positive_check
+
     ActiveRecord::Migration.add_positive_check :products, :price
-    begin
-      Product.create(:price => -1)
-    rescue Exception => error
-      assert_match /PGError/, error.message
-      assert_match /violates check constraint "valid_price"/, error.message
-    end
+
     Product.create(:price => 1)
     assert_equal 1, Product.count
+
+    [ 0, -1 ].each do |value|
+
+      begin
+        Product.create :price => value
+      rescue Exception => error
+        assert_match /PGError/, error.message
+        assert_match /violates check constraint "valid_price"/, error.message
+      end
+
+    end
+
   end
 
 end
